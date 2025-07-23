01c1587da69dc98c3f0edcd9843a5da751411dfe
keneogu <kc101com@gmail.com>
2025-07-23 20:51:44 +0100

    Add, configure and implement zipkin for distributed tracing across the microservices

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -3,6 +3,9 @@ package com.ecommerce.user.controller;
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +15,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
   @Autowired
   UserService userService;
+//  private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
   @GetMapping
   public ResponseEntity<List<UserResponse>> getAllUsers(){
@@ -26,6 +31,7 @@ public class UserController {
 
   @GetMapping("/{id}")
   public ResponseEntity<UserResponse> getUser(@PathVariable String id){
+    log.info("Request received for user: {}", id);
     return userService.fetchUser(id)
         .map(ResponseEntity::ok)
         .orElseGet(() -> ResponseEntity.notFound().build());
